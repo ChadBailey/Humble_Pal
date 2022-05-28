ee65488e92f2e3dfa07fbac018be8f11642c048c
Chad Bailey <chadbailey904@gmail.com>
2022-05-28 17:30:04 -0500

    Added snippets.js to store browser console copy+pastable scripts

---
+++ humble_pal/steam/snippets.js
@@ -0,0 +1,5 @@
+// This snippet will change all orange "CLAIMED" banners to say "test"
+// This is useful for a potential feature which changes "CLAIMED" to the
+// account claimed to
+ 
+// for (let node of document.querySelectorAll("div.claimed-text")) {node.innerText = "test"};
